e11aba1216f6ae824496766e05205e9be0ec3096
Carlos Henriquez <carlosh@mit.edu>
2018-04-25 05:15:29 -0400

    Make motor vibrate when hovering over graph

--- js/index.js
+++ js/index.js
@@ -1,84 +1,69 @@
-function createPlot(){
-        try {
-    // compile the expression once
-        var expression = document.getElementById('eq').value;
-        var expr = math.compile(expression);
-
-        // evaluate the expression repeatedly for different values of x
-        var xValues = math.range(-10, 10, 0.1).toArray();
-        var yValues = xValues.map(function (x) {
-            return expr.eval({x: x});
-        });
-
-        // render the plot using plotly
-      
-        var plot = document.getElementById('plot')
-        var data = [{x: xValues,
-                    y: yValues,
-                    type: 'scatter'
-                    }]
-        var layout = {hovermode:'closest',
-                    hoverdistance: 25};
-
-        return [plot, data, layout]
-    }
-    catch (err) {
-        console.error(err);
-        alert(err);
-    }
-    }
-function draw(data, layout) {
-
-    Plotly.newPlot('plot', data, layout);
-
-}
-var plotInfo = createPlot();
-
-
-document.getElementById('form').onsubmit = function (event) {
-    event.preventDefault();
-    plotInfo = createPlot();
-    funcPlot = plotInfo[0]
-    data = plotInfo[1]
-    layout = plotInfo[2]
-    draw(data, layout);
-};
-
-var funcPlot = plotInfo[0]
-var data = plotInfo[1]
-var layout = plotInfo[2]
-draw(data, layout);
-
 // Make sure you have the leap motion service running otherwise you'll get an error
-var webSocket = require('ws'),
-ws = new webSocket('ws://127.0.0.1:6437'),
-SerialPort = require("serialport"),
-five = require('johnny-five'),
-board = new five.Board(),
-console.log(SerialPort);
-//COM_PORT = new SerialPort("COM7", {baudrate: 9600, buffersize: 1}),
-var port = new SerialPort("/dev/ttyS7", {baudRate: 9600});
-var board = new five.Board({port: port}),
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var webSocket = require('ws');
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+    console.log("Serving " + __dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+    console.log('connected to websocket');
+    socket.on('disconnect', function() {
+        console.log('websocket disconnected');
+    });
+
+    socket.on('line hover', function(msg) {
+        console.log("hovering over a line " + msg.data);
+        if (led) {
+            led.on();
+        }
+    });
+
+    socket.on('line unhover', function(msg) {
+        console.log("unhovering over a line " + msg.data);
+         if (led) {
+            led.off();
+        }
+    });
+
+});
+
+var ws = new webSocket('ws://127.0.0.1:6437'),
+    SerialPort = require("serialport"),
+    five = require('johnny-five'),
+    port = new SerialPort("/dev/ttyS5"),
+    board = new five.Board({port: port}),
     led, frame;
+//console.log(SerialPort);
+//COM_PORT = new SerialPort("COM7", {baudrate: 9600, buffersize: 1}),
+SerialPort.list(function (err, ports) {
+		console.log(err,ports);
+});
+//var port = new SerialPort("/dev/ttyS7", {baudRate: 9600});
+//var board = new five.Board({port: port}),
+//    led, frame;
 
 // Will need to address Windows/OSX/Linux ports https://github.com/rwaldron/johnny-five/wiki/Board
-SerialPort.list(function (err, ports) {
-		console.log(ports);
-});
 
 
 board.on('ready', function() {
     led = new five.Led(13);    
-    ws.on('message', function(data, flags) {
+    /*ws.on('message', function(data, flags) {
         frame = JSON.parse(data); 
-        funcPlot.on('plotly_hover', function(data){
-
-            hoverInfo.innerHTML = 'hovering';
+        if (frame.hands && frame.hands.length == 1) {
             led.on();
-        })
-        .on('plotly_unhover', function(data){
-            hoverInfo.innerHTML = 'not hovering';
+        } else {
             led.off();
-        });
-    });
+        }
+    });*/
+});
+
+http.listen(3000, function() {
+    console.log("listening on *:3000");
 });
