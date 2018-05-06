ab80f7a727c93d79e347cbc3d2c61f30c746bbfd
Carlos Henriquez <carlosh@mit.edu>
2018-05-06 17:51:12 -0400

    Configure Arduino to use mutliple motors if available
    TODO: Add extra invisible lines to D3 chart to figure out scaled vibrations

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ io.on('connection', function(socket) {
         console.log('websocket disconnected');
     });
 
-    socket.on('line hover', function(msg) {
+/*    socket.on('line hover', function(msg) {
         console.log("hovering over a line ");
         if (led) {
             led.on();
@@ -30,6 +30,62 @@ io.on('connection', function(socket) {
          if (led) {
             led.off();
         }
+    });*/
+
+    socket.on('thumb enter', function() {
+        console.log('Thumb is hovering over line');
+        if (thumbLed) {
+            thumbLed.on();
+        }
+    });
+
+    socket.on('thumb leave', function() {
+        console.log("Thumb left");
+        if (thumbLed) {
+            thumbLed.off();
+        }
+    });
+
+    socket.on('index enter', function() {
+        console.log('Index is hovering over line');
+        if (indexLed) {
+            indexLed.on();
+        }
+    });
+
+    socket.on('index leave', function() {
+        console.log("Index left");
+        if (indexLed) {
+            indexLed.off();
+        }
+    });
+
+    socket.on('middle enter', function() {
+        console.log('middle is hovering over line');
+        if (middleLed) {
+            middleLed.on();
+        }
+    });
+
+    socket.on('middle leave', function() {
+        console.log("middle left");
+        if (middleLed) {
+            middleLed.off();
+        }
+    });
+
+    socket.on('ring enter', function() {
+        console.log('ring is hovering over line');
+        if (ringLed) {
+           ringLed.on();
+        }
+    });
+
+    socket.on('ring leave', function() {
+        console.log("ring left");
+        if (ringLed) {
+            ringLed.off();
+        }
     });
 
 });
@@ -39,20 +95,23 @@ var ws = new webSocket('ws://127.0.0.1:6437'),
     five = require('johnny-five'),
 //    port = new SerialPort("/dev/ttyS11"),
 //    board = new five.Board({port: port}),
-    led, frame;
+    thumbLed, indexLed, middleLed, ringLed, frame;
 //console.log(SerialPort);
-//COM_PORT = new SerialPort("COM7", {baudrate: 9600, buffersize: 1}),
+//COM_PORT = new SerialPort("COM5", {baudrate: 9600, buffersize: 1}),
 SerialPort.list(function (err, ports) {
 		console.log(ports);
 });
-//var port = new SerialPort("/dev/ttyS7", {baudRate: 9600});
-//var board = new five.Board({port: port}),
+var port = new SerialPort("/dev/ttyS5", {baudRate: 9600});
+var board = new five.Board({port: port});
 //    led, frame;
 
 // Will need to address Windows/OSX/Linux ports https://github.com/rwaldron/johnny-five/wiki/Board
-//board.on('ready', function() {
-//    led = new five.Led(12);    
-//});
+board.on('ready', function() {
+   thumbLed = new five.Led(12);
+   indexLed = new five.Led(11);
+   middleLed = new five.Led(10);
+   ringLed = new five.Led(9);
+});
 
 http.listen(3000, function() {
     console.log("listening on *:3000");
